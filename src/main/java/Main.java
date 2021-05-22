3eb0012f40fd362cd3f97956467126cb787ff50f
emilpyp <emilnah@code.edu.az>
2021-05-22 19:47:03 +0400

    fixed issue with update and delete student

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import Utils.InputReader;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import models.Student;
 
 import java.io.*;
@@ -15,6 +16,7 @@ public class Main {
     private static SortedMap<String, Student> surnameStudents = new TreeMap<>();
     private static SortedMap<String, Student> fatherNameStudents = new TreeMap<>();
     private static int id = 0;
+    private static String dash = "-".repeat(67);
 
     public static void main(String[] args) throws IOException {
         getFileData();
@@ -40,13 +42,13 @@ public class Main {
     }
 
     private static int menu(){
-        System.out.println("-------------------------------------------------------------------STUDENT MENU-------------------------------------------------------------------");
+        System.out.println(dash + "STUDENT MENU" + dash);
         System.out.println("1.CREATE A STUDENT");
         System.out.println("2.UPDATE A STUDENT");
         System.out.println("3.DELETE A STUDENT");
         System.out.println("4.SEARCH OVER STUDENTS");
         System.out.println("5.LOG OUT");
-        System.out.println("-------------------------------------------------------------------STUDENT MENU-------------------------------------------------------------------");
+        System.out.println(dash + "STUDENT MENU" + dash);
         return InputReader.readInt("Enter number of menu: ",1, 5);
     }
 
@@ -76,12 +78,12 @@ public class Main {
     }
 
     private static int searchMenu(){
-        System.out.println("-------------------------------------------------------------------SEARCH MENU-------------------------------------------------------------------");
+        System.out.println(dash + "SEARCH MENU" + dash);
         System.out.println("1.SEARCH BY NAME");
         System.out.println("2.SEARCH BY SURNAME");
         System.out.println("3.SEARCH BY FATHER NAME");
         System.out.println("4.GO BACK");
-        System.out.println("-------------------------------------------------------------------SEARCH MENU-------------------------------------------------------------------");
+        System.out.println(dash + "SEARCH MENU" + dash);
         return InputReader.readInt("Enter number of menu: ",1, 4);
     }
 
@@ -106,7 +108,7 @@ public class Main {
 
 
     public static void createStudent() throws JsonProcessingException {
-        System.out.println("-------------------------------------------------------------------CREATE STUDENT-------------------------------------------------------------------");
+        System.out.println(dash + "CREATE STUDENT" + dash);
         String name = InputReader.readString("Enter name of the student : ");
         String surName = InputReader.readString("Enter surname of the student : ");
         String fatherName = InputReader.readString("Enter father name of the student : ");
@@ -120,40 +122,46 @@ public class Main {
     }
 
     public static void updateStudent() throws JsonProcessingException {
-        System.out.println("-------------------------------------------------------------------UPDATE STUDENT-------------------------------------------------------------------");
-        int id = InputReader.readInt("Enter id of student: ", 0 , Integer.MAX_VALUE);
-        if (!checkIfExists(id)){
-            updateStudent();
-        }
-        Student student = getStudent(id);
-        System.out.println("You are updating: " +student.toString());
-        String name = InputReader.readString("Enter new name of the student : ");
-        String surName = InputReader.readString("Enter new surname of the student : ");
-        String fatherName = InputReader.readString("Enter new father name of the student : ");
-        String email = InputReader.emailReader("Enter new email of the student : ");
-        String phoneNumber = InputReader.phoneReader("Enter new phone number of the student : ");
+        while(true) {
+            System.out.println(dash + "UPDATE STUDENT" + dash);
+            int id = InputReader.readInt("Enter id of student: ", 0, Integer.MAX_VALUE);
+            if (!checkIfExists(id)) {
+                continue;
+            }
+            Student student = getStudent(id);
+            System.out.println("You are updating: " + student.toString());
+            String name = InputReader.readString("Enter new name of the student : ");
+            String surName = InputReader.readString("Enter new surname of the student : ");
+            String fatherName = InputReader.readString("Enter new father name of the student : ");
+            String email = InputReader.emailReader("Enter new email of the student : ");
+            String phoneNumber = InputReader.phoneReader("Enter new phone number of the student : ");
 
-        student.setName(name);
-        student.setSurName(surName);
-        student.setFatherName(fatherName);
-        student.setEmail(email);
-        student.setPhone(phoneNumber);
-        updateJsonFile();
-        syncWithList();
-        System.out.println("Student updated successfully");
+            student.setName(name);
+            student.setSurName(surName);
+            student.setFatherName(fatherName);
+            student.setEmail(email);
+            student.setPhone(phoneNumber);
+            updateJsonFile();
+            syncWithList();
+            System.out.println("Student updated successfully");
+            return;
+        }
     }
 
     public static void deleteStudent() throws JsonProcessingException {
-        System.out.println("-------------------------------------------------------------------DELETE STUDENT-------------------------------------------------------------------");
-        int id = InputReader.readInt("Enter id of student: ", 0 , Integer.MAX_VALUE);
-        if (!checkIfExists(id)){
-            deleteStudent();
+        while (true) {
+            System.out.println(dash + "DELETE STUDENT" + dash);
+            int id = InputReader.readInt("Enter id of student: ", 0, Integer.MAX_VALUE);
+            if (!checkIfExists(id)) {
+                continue;
+            }
+            Student student = getStudent(id);
+            students.remove(student);
+            updateJsonFile();
+            syncWithList();
+            System.out.println("Student deleted successfully");
+            return;
         }
-        Student student = getStudent(id);
-        students.remove(student);
-        updateJsonFile();
-        syncWithList();
-        System.out.println("Student deleted successfully");
     }
 
     public static void searchByName(){
@@ -206,6 +214,7 @@ public class Main {
 
     public static void updateJsonFile() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         String jsonString = mapper.writeValueAsString(students);
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream("students.json"), "utf-8"))) {
